974ba40fe26274c71629d73ee995e0703f4d8e4c
Magdalena Panasiuk <magdalena.panasiukk@gmail.com>
2016-11-28 12:38:25 +0100

    komentarz

--- fTrains/ServicesHost/Contracts/ITicketService.cs
+++ fTrains/ServicesHost/Contracts/ITicketService.cs
@@ -13,7 +13,7 @@ namespace ServicesHost.Contracts
         [TransactionFlow(TransactionFlowOption.Allowed)]
         List<Ticket> MakeReservation(List<Connection> connections, User user,int no=1,Ticket.Discount discount=Ticket.Discount.Without);
         [OperationContract]
-        List<List<Connection>> FindRoute(Station stationDeparture, Station stationArrival, DateTime dateTimeDeparture);
+        List<List<Connection>> FindRoute(Station stationDeparture, Station stationArrival, DateTime dateTimeDeparture);// tu nie ma flow bo nie ma na bazie 
         [OperationContract]
         List<Ticket> UsersTickets(User user);
         [OperationContract]
