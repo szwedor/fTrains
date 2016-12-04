b37bb8df39a6ee2dc018c7abe36181b1b642b76a
Magdalena Panasiuk <magdalena.panasiukk@gmail.com>
2016-12-05 00:48:18 +0100

    user

---
+++ fTrains/Service/App_Data/Contracts/IUserManagment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using DomainModel.Models;
+
+namespace Service.App_Data.Contracts
+{
+    [ServiceContract]
+    public interface IUserManagmentUnsecure
+    {
+        [OperationContract]
+        void AddUser(User a);
+    }
+    [ServiceContract]
+    public interface IUserManagment : IReservationManagmentUnsecure
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void Login(string login, string pass);
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void ChangePass(string login, string oldpass, string newpass);
+    }
+}
---
+++ fTrains/Service/UserManagment.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Autofac;
+using DomainModel;
+using DomainModel.Models;
+using Service.App_Data.Contracts;
+
+namespace Service
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall,
+                  ConcurrencyMode = ConcurrencyMode.Multiple,
+                  ReleaseServiceInstanceOnTransactionComplete = false)]
+    public class UserManagment : IUserManagment,IUserManagmentUnsecure
+
+    {
+        public UserManagment()
+        {
+
+            Bootstrap.BuildContainer();
+        }
+
+        public void AddUser(User a)
+        {
+            User _user;
+            using (var scope = Bootstrap.Container.BeginLifetimeScope())
+            {
+                IUnitOfWork u = scope.Resolve<IUnitOfWork>();
+                a.Email = a.Email.ToLower();
+                _user = a;
+                u.StartTransaction();
+                u.UsersRepository.Add(a);
+                u.Save();
+                u.EndTransaction();
+            }
+        }
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void ChangePass(string login,string oldpass,string newpass)
+        {
+            using (var scope = Bootstrap.Container.BeginLifetimeScope())
+            {
+                IUnitOfWork u = scope.Resolve<IUnitOfWork>();
+
+            }
+        }
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void Login(string userName, string pass)
+        {
+        }
+
+    }
+}
+
